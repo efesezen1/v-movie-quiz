e8765143d557d25104b99b411ba74da074d9eacc
Efe <efesezen1@outlook.com>
2024-10-15 10:38:26 +0300

    Done for netlify build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import VitePluginVueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/v-movie-quiz/',
+   // base: '/v-movie-quiz/',
    plugins: [vue(), VitePluginVueDevTools()],
    resolve: {
       alias: {
